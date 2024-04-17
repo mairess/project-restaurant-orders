9c85233c4cf2e08c126abe229bb810f2b2956b92
mairess <trabalho.maires@gmail.com>
2024-04-17 18:16:53 -0300

    test: class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    chicken = Ingredient("frango")
+    shrimp = Ingredient("camarão")
+    other_shrimp = Ingredient("camarão")
+
+    assert chicken.__eq__(shrimp) is False
+
+    assert shrimp.name == "camarão"
+
+    assert shrimp.__hash__() != chicken.__hash__()
+
+    assert shrimp.__hash__() == other_shrimp.__hash__()
+
+    assert shrimp.__eq__(other_shrimp) is True
+
+    assert shrimp.__repr__() == "Ingredient('camarão')"
+
+    assert shrimp.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
